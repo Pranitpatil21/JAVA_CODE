56a05162d82c71393488dec83046824e7ea1b634
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2025-12-26 14:42:07 +0530

    P2Sum.java
    Fixed formatting issues in P2Sum.java.

--- P2Sum.java
+++ P2Sum.java
@@ -1,6 +1,6 @@
 // Print The Row-Wise Sum of the etire matrix
 
-class code1{
+class code1{ 
     public static void main(String[] args){
         int arr[][] = new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 
