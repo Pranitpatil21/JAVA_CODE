5bd060aa8a3ee5b3c78d64fcd00592f17a720558
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2025-12-26 14:42:28 +0530

    3) InsertionSort.java

--- 3) InsertionSort.java
+++ 3) InsertionSort.java
@@ -4,7 +4,7 @@ class InsertionSort{
     public static void main(String[] args){
         int [] arr =  new int[] {8,3,1,7,5,4,2};
         for(int i = 1; i < arr.length ; i++){
-            int element = arr[i];
+            int element = arr[i]; 
             int j = i-1;
             while(j>=0 && arr[j]>element){
                 arr[j+1] =arr[j];
