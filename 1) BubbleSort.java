119a09af8b2478e4cf503476df6867a6f420ea87
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2026-01-10 02:27:40 +0530

    1) BubbleSort.java

--- 1) BubbleSort.java
+++ 1) BubbleSort.java
@@ -7,7 +7,7 @@ class Searching{
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j + 1];
+                    arr[j] = arr[j + 1]; 
                     arr[j + 1] = temp;
                 }
             }
