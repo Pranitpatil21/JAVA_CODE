7ad895fd7d463af45e8ad1040b1776fdad84a079
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2026-01-17 22:28:15 +0530

    Implement first occurrence search in BinarySearch
    Modified binary search function to find the first occurrence of an element in a sorted array.

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,7 +2,7 @@
    Modify the binary search function to find the first occurrence of a given element in
    a sorted array. Return -1 if the element is not present.
    For example, given the input array arr = [2, 4, 4, 4, 6, 7, 8] and the target element
-   4, the function should return 1, as 4 first appears at index 1 in the array.   */
+   4, the function should return 1, as 4 first appears at index 1 in the array.   */ 
  
 
 import java.util.*;
