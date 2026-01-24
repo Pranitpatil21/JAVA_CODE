984f5e034ca2a712347019eac8b733852cc6b39e
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2026-01-24 12:39:05 +0530

    5) SelectionSort.java

--- 5) SelectionSort.java
+++ 5) SelectionSort.java
@@ -4,7 +4,7 @@ class SelecetionSort{
     public static void main(String[] args){
         int[] arr = new int[]{9,2,7,3,8,4,1,6};
         for(int i=0;i<arr.length-1;i++){
-            int minindex = i;
+            int minindex = i; 
             for(int j=i+1;j<arr.length;j++){
                 if(arr[j]<arr[minindex]){  //Finding the Minimumm Element
                     minindex = j;
